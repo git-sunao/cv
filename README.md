c0443e29579bdee2320697cdcf3cbf784d3925be
git-sunao <sunaosugiyama@gmail.com>
2023-04-15 19:16:45 +0900

    updated

--- README.md
+++ README.md
@@ -15,10 +15,9 @@ latexmk -pdfdvi cv
 2. Then edit bst file to include adsurl (see https://github.com/yangcht/AA-bibstyle-with-hyperlink/blob/master/aa_url.bst).
 
 ## How to make a publication list
-
 1. Go to NASA [ADS](<https://ui.adsabs.harvard.edu/search/filter_author_facet_hier_fq_author=AND&filter_author_facet_hier_fq_author=author_facet_hier%3A%221%2FSugiyama%2C%20S%2FSugiyama%2C%20Sunao%22&fq=%7B!type%3Daqp%20v%3D%24fq_author%7D&fq_author=(author_facet_hier%3A%221%2FSugiyama%2C%20S%2FSugiyama%2C%20Sunao%22)&q=pubdate%3A%5B2001-01%20TO%209999-12%5D%20author%3A(%22Sugiyama%2CSunao%22)&sort=date%20desc%2C%20bibcode%20desc&p_=0>)
-2. Export the list to a bibtex file.
-3. Edit the bib file: add contribution and alphabetical attributes.
+2. Export the list to a bibtex file. If this is the first time to make publication list, then save the bib file as "publists/refs.bib", otherwise, "publists/refs2.bib".
+3. Run `python process.py`, then `refs.bib` is updated.
 4. Run `python make_publicationlist.py`, then `publicationlist.tex` is generated.
 
 ## How to make a talk list
--- processbib.py
+++ processbib.py
@@ -3,7 +3,10 @@ Copyright Sunao Sugiyama
 """
 
 import bibtexparser # https://bibtexparser.readthedocs.io/en/master/
-import numpy as np
+import os
+
+myname1 = 'Sugiyama'
+myname2 = 'Sunao'
 
 def load_bib(fname):
     with open(fname) as bibtex_file:
@@ -15,13 +18,59 @@ def dump_bib(fname, bib):
         bibtexparser.dump(bib, f)
 
 def emphasize_myname(bib):
-    for i in range(len(bib.entries)):
-        if '\myname' in bib.entries[i]['author']:
+    for i, entry in enumerate(bib.entries):
+        if '\myname' in entry['author']:
             continue
-        bib.entries[i]['author'] = bib.entries[i]['author'].replace('{Sugiyama}, Sunao', '\myname{{Sugiyama}, Sunao}')
+        bib.entries[i]['author'] = entry['author'].replace('{%s}, %s'%(myname1, myname2), '\myname{{%s}, %s}'%(myname1, myname2))
     return bib
 
+def add_contribution(bib):
+    for i, entry in enumerate(bib.entries):
+        if 'contribution' in entry:
+            continue
+        print(f"title {entry['title']}")
+        c = input(f'Major contribution? (y/n, default=y)') or 'y'
+        bib.entries[i]['contribution'] = 'major' if c == 'y' else 'minor'
+    return bib
+
+def add_alphabetical(bib):
+    for i, entry in enumerate(bib.entries):
+        if 'alphabetical' in entry:
+            continue
+        print(f"title {entry['title']}")
+        c = input(f'Alphabetical order? (y/n, default=n)') or 'n'
+        bib.entries[i]['alphabetical'] = 'false' if c == 'n' else 'true'
+    return bib
+
+def merge_bibs(bib1, bib2, identifier='eprint'):
+    """
+    Merges two bibs. The first is the base bib, and the second is the new one.
+    """
+
+    bib1_ids = [entry[identifier] for entry in bib1.entries]
+
+    for i, entry in enumerate(bib2.entries):
+        if entry[identifier] in bib1_ids:
+            continue
+        print(f"New entry: {entry['title']}")
+        bib1.entries.append(entry)
+
 if __name__ == '__main__':
-   bib = load_bib('publists/refs.bib')
-   bib = emphasize_myname(bib)
-   dump_bib('publists/refs.bib', bib)
+    # existing bib
+    bib = load_bib('publists/refs.bib')
+    
+    # new bib file
+    if os.path.exists('publists/refs2.bib'):
+        print('>>> Adding bib entries from publists/refs2.bib to publists/refs.bib')
+        bib2 = load_bib('publists/refs2.bib')
+        merge_bibs(bib, bib2)
+
+    # updates
+    print('>>> Adding contribution to entries.')
+    bib = add_contribution(bib)
+    print('>>> Adding alphabetical or not to entries.')
+    bib = add_alphabetical(bib)
+    bib = emphasize_myname(bib)
+
+    # write
+    dump_bib('publists/refs.bib', bib)
