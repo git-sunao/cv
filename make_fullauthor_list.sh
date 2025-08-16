50a0a72b2d706a46af573b6a7b49e052f3afd6da
git-sunao <sunaosugiyama@gmail.com>
2025-08-15 20:27:41 -0400

    added full author list

---
+++ make_fullauthor_list.sh
@@ -0,0 +1,6 @@
+
+sed 's/etalstyle/fullauthor/g' en/publist.tex > en/publist_fullauthor.tex
+sed 's/etalstyle/fullauthor/g' ja/publist.tex > ja/publist_fullauthor.tex
+
+sed 's/publist/publist_fullauthor/g' en/sunao_type_list.tex > en/sunao_type_list_fullauthor.tex
+sed 's/publist/publist_fullauthor/g' ja/sunao_type_list.tex > ja/sunao_type_list_fullauthor.tex
