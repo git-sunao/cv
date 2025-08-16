a71a926fe1a6fe438ea199c6ebb1b091661be047
git-sunao <sunaosugiyama@gmail.com>
2025-08-15 20:05:02 -0400

    updates

--- processbib.py
+++ processbib.py
@@ -5,8 +5,8 @@ Copyright Sunao Sugiyama
 import bibtexparser # https://bibtexparser.readthedocs.io/en/master/
 import os
 
-myname1 = 'Sugiyama'
-myname2 = 'Sunao'
+myname1 = 'Sunao'
+myname2 = 'Sugiyama'
 
 def load_bib(fname):
     with open(fname) as bibtex_file:
@@ -49,7 +49,7 @@ def print_bib_entry(bib_entry):
     for key, val in bib_entry.items():
         print(key.ljust(nmax+5), '|', val)
 
-def merge_bibs(bib1, bib2, identifier='eprint', update=False):
+def merge_bibs(bib1, bib2, identifier='eprint', update=True):
     """
     Merges two bibs. The first is the base bib, and the second is the new one.
     """
@@ -64,13 +64,28 @@ def merge_bibs(bib1, bib2, identifier='eprint', update=False):
             if update:
                 bib1_idx = bib1_ids.index(entry[identifier])
                 entry_bib1 = bib1.entries[bib1_idx]
-                for key, val in entry.items():
-                    if key in entry_bib1: continue
-                    entry_bib1[key] = val
+                # for key, val in entry.items():
+                #     if key in entry_bib1: continue
+                #     entry_bib1[key] = val
+                entry_bib1.update(entry)
                 bib1.entries[bib1_idx] = entry_bib1
         else:
             print(f"New entry: {entry['title']}")
             bib1.entries.append(entry)
+
+def fix_by_hand(bib):
+    replacements = [("title", "<inline-formula><mml:math display=``inline''><mml:msub><mml:mi>S</mml:mi><mml:mn>8</mml:mn></mml:msub></mml:math></inline-formula>", "$S_8$")]
+
+    for replacement in replacements:
+        key, s_dept, s_dest = replacement
+        for entry in bib.entries:
+            if key not in entry:
+                continue
+            if not isinstance(entry[key], str):
+                continue
+            entry[key] = entry[key].replace(s_dept, s_dest)
+    return bib
+
 
 if __name__ == '__main__':
     # existing bib
@@ -88,6 +103,7 @@ if __name__ == '__main__':
     print('>>> Adding alphabetical or not to entries.')
     bib = add_alphabetical(bib)
     bib = emphasize_myname(bib)
+    bib = fix_by_hand(bib)
 
     # write
     dump_bib('publists/refs.bib', bib)
